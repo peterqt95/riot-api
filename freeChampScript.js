027f1381eb94cc2398035dff907d31054d524281
Peter Tran <iarept@gmail.com>
2015-09-13 21:00:51 -0700

    basic skill information

--- freeChampScript.js
+++ freeChampScript.js
@@ -17,7 +17,7 @@ function findChamp(){
 				getChamp(champion, i);
 			}
 		},
-		error: function(XMLHttpReqest, textStatus, errorThrown){
+		error: function(XMLHttpRequest, textStatus, errorThrown){
 			alert("something went wrong");
 		}
 	});
@@ -41,10 +41,96 @@ function getChamp(championIDList, i){
 			document.getElementById(currentChampTitle).innerHTML = currentChamp.title; //set Title
 			document.getElementById(currentImgNav).src = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/'+champName+'.png'; //set nav img
 			document.getElementById(currentImgBG).src = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+champName+'_0.jpg'; //set bg img
+
+			//Set current Champion Passive
+			getPassive(championIDList[i], i);
+			getSkills(championIDList[i], i);
 		},
-		error: function(XMLHttpReqest, textStatus, errorThrown){
+		error: function(XMLHttpRequest, textStatus, errorThrown){
 			alert("champ name fail");
 		}
+	});
+}
+
+function getPassive(championID, curIndex){
+	$.ajax({
+		url: 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + championID + '?champData=passive&api_key=093e1934-1a18-490c-8062-1ec8df1b5923',
+		type: 'GET',
+		dataType: 'json',
+		data: {},
+		success: function(currentChamp){
+			//Elements to fill in document
+			var passive_name = currentChamp.passive.name;
+			var passive_img = currentChamp.passive.image.full;
+			var passive_desc = currentChamp.passive.description;
+
+			//Find the correct selector
+			var index = curIndex.toString();
+			var doc_id_passive = 'passive'.concat(index);
+			var doc_id_passive_name = doc_id_passive.concat('_name');
+			var doc_id_passive_img = doc_id_passive.concat('_img');
+			var doc_id_passive_desc = doc_id_passive.concat('_desc');
+
+			//Assign values to appropriate selector
+			document.getElementById(doc_id_passive_name).innerHTML = passive_name;
+			document.getElementById(doc_id_passive_img).src = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/passive/' + passive_img;
+			document.getElementById(doc_id_passive_desc).innerHTML = passive_desc;
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown){
+			alert("champ passive fail");
+		}
+	});
+}
+
+function getSkills(championID, curIndex){
+	$.ajax({
+		url: 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + championID + '?champData=spells&api_key=093e1934-1a18-490c-8062-1ec8df1b5923',
+		type: 'GET',
+		dataType: 'json',
+		data: {},
+		success: function(currentChamp){
+			for(var j = 0; j < 4; j++){
+				/* Elements to fill DOM */
+
+				//name
+				var skill_name = currentChamp.spells[j].name;
+				//Determine skill range
+				var skill_range = "Range: " + currentChamp.spells[j].rangeBurn;
+				//Determine skill cost
+				var skill_cost = currentChamp.spells[j].costType + ": " + currentChamp.spells[j].costBurn;
+				//Determine skill CD
+				var skill_cd = "Cooldown: " + currentChamp.spells[j].cooldownBurn;
+				//image
+				var skill_image = currentChamp.spells[j].image.full;
+
+				/* Getting correct selector for DOM */
+
+				var index = curIndex.toString();
+				var spellIndex = j.toString();
+				var curChamp = 'champ'.concat(index);
+				var doc_id_skill = curChamp.concat('_skill');
+				doc_id_skill = doc_id_skill.concat(spellIndex);
+				var doc_id_skill_name = doc_id_skill.concat('_name');
+				var doc_id_skill_img = doc_id_skill.concat('_img');
+				var doc_id_skill_range = doc_id_skill.concat('_range');
+				var doc_id_skill_cost = doc_id_skill.concat('_cost');
+				var doc_id_skill_cd = doc_id_skill.concat('_cd');
+				var doc_id_skill_active = doc_id_skill.concat('_active');
+
+
+				/* Setting Elements */
+				document.getElementById(doc_id_skill_name).innerHTML = skill_name;
+				document.getElementById(doc_id_skill_img).src = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/spell/' + skill_image;
+				document.getElementById(doc_id_skill_cost).innerHTML = skill_cost;
+				document.getElementById(doc_id_skill_range).innerHTML = skill_range;
+				document.getElementById(doc_id_skill_cd).innerHTML = skill_cd;
+				document.getElementById(doc_id_skill_active).innerHTML = currentChamp.spells[j].description;
+			}
+
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown){
+			alert("champ skill name fail");
+		} 
 	});
 }
 
@@ -100,9 +186,53 @@ function newImage(index){
 	refresh_time = setTimeout(function(){ newImage('next'); }, image_change_speed);
 };
 
+/* View skills */
+var $active = $('.passive'),
+	$oldTab;
+var whichClicked = 'passive',
+	newClick = '';
+
+	$oldTab = $('nav a:first-of-type');
+function tabSwitch() {
+		$('.tab').on('click', function(event) {
+		event.preventDefault();                          // Prevent link being followed
+        newClick = $( this ).text().toLowerCase();
+    	switch(newClick){
+    		case '1':
+    			newClick = 'skill0';
+    			break;
+    		case '2':
+    			newClick = 'skill1';
+    			break;
+    		case '3':
+    			newClick = 'skill2';
+    			break;
+    		case '4':
+    			newClick = 'skill3';
+    			break;
+    		default:
+    			newClick = 'passive';
+    			break;
+    	}
+        if ( newClick !== whichClicked ) {
+        	$oldTab.removeClass('selected');
+            $oldTab = $( this );
+
+            $active.css( 'display', 'none' );           // Hide the last active pane
+
+            $active = $( '.' + newClick );              // Set variable that indicates what the currently active pane is
+
+            $active.css( 'display', 'block' );          // Display the new currently active pane
+            whichClicked = newClick;
+
+            $( this ).addClass( 'selected' );
+        }
+	});
+}
 
 $(document).ready(function() {
 	findChamp();
 	//start slider
-	newImage('next');
+	//newImage('next');
+	tabSwitch();
 })
